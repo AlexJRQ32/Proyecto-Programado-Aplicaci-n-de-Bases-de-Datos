5c5e6aa70b69bd4fdd9b9e9c76ce260f3770705d
AlexJRQ32 <robleroalex015@icloud.com>
2026-04-16 07:50:23 -0600

    Actualizacion del Proyecto

---
+++ 0. Limpiar Usuarios.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------
+-- Esto se ejecuta con el usuario SYS.
+--------------------------------------------------------------------------------
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+DROP USER SISVENTAS_ER CASCADE;
+DROP USER SISVENTAS_SA CASCADE;
+DROP USER SISVENTAS_DW CASCADE;
